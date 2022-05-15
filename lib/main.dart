143cb8e4ef20b8db222c032ba76f0b1b57f36767
George <george.hs@hsress.com>
2022-05-15 11:30:22 +0800

    Design theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,22 @@ import 'package:flutter/material.dart';
 import './screens/products_overview_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final ThemeData theme =
+      ThemeData(primarySwatch: Colors.purple, fontFamily: 'Lato');
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: theme.copyWith(
+          colorScheme:
+              theme.colorScheme.copyWith(secondary: Colors.deepOrange)),
       home: ProductsOverviewScreen(),
     );
   }
