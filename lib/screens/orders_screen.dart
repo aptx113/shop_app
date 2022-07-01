f22e42bb54de3e897aa84f7ca562495e8cc9c269
George <george.hs@hsress.com>
2022-07-01 09:39:22 +0800

    Obtain future single time

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,21 +5,36 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/shop_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   static const routeName = '/orders';
 
   @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  late Future _ordersFuture;
+
+  Future _obtainOrdersFuture() =>
+      Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
       drawer: const ShopDrawer(),
       body: FutureBuilder(
-        future: orders.fetchAndSetOrders(),
+        future: _ordersFuture,
         builder: (context, dataSnapshot) {
           if (dataSnapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
